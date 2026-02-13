14e5b09677f779830e04ada1b09eed03341c24e7
parth8367 <sorathiyaparth762@gmail.com>
2026-02-14 01:42:49 +0530

    Fixed memory section layout structure

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "assets\music_1.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dolysh3vw/video/upload/v1771002850/music_1_a210mu.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
